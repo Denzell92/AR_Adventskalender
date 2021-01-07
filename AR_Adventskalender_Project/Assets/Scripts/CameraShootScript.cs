e7297e6f67b6ffbbb70597a972edbf7c9d2053de
Denzell92 <s0558978@htw-berlin.de>
2021-01-07 21:08:56 +0100

    Bullet implemented

---
+++ AR_Adventskalender_Project/Assets/Scripts/CameraShootScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShootScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int bulletSpeed = 50;
+    public GameObject bullet;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            /*
+            GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            bullet.transform.position = new Vector3(0, 0, -5);
+            bullet.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+            bullet.AddComponent<Rigidbody>();
+            */
+
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            GameObject newBullet = (GameObject)Instantiate(bullet, mouseRay.origin, Quaternion.identity);
+            Rigidbody rb = newBullet.GetComponent<Rigidbody>();
+
+
+            rb.velocity = mouseRay.direction * bulletSpeed;
+        }
+    }
+}
