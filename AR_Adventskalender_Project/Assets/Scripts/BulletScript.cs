43888b5cf01bf19acb513ff60175adc9c3f2ed55
Denzell92 <s0558978@htw-berlin.de>
2021-01-09 18:11:49 +0100

    added enemies with movement and bullets to shoot enemies also with movement

---
+++ AR_Adventskalender_Project/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ist okay aber noch fehlerbehaftet
+        if (transform.position.y < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ AR_Adventskalender_Project/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    float speed = 1;
+    public GameObject target;
+    int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement funktioniert, eventuell ausgleichen des Wackelns
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z), step);
+
+        checkHealth();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        health -= 25;
+    }
+
+    private void checkHealth()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
