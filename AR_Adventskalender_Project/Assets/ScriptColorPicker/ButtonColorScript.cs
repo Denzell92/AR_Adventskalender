60cb1d9f5f5831c0e3f06189a27f31964b582ca2
Denzell92 <s0558978@htw-berlin.de>
2021-01-11 14:09:47 +0100

    ColorPicker works, 3D-Models are not included yet

---
+++ AR_Adventskalender_Project/Assets/ScriptColorPicker/ButtonColorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonColorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Material colorMaterial;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void respond()
+    {
+        List<ObjectColorScript> scriptList = new List<ObjectColorScript>();
+        scriptList.AddRange(FindObjectsOfType(typeof(ObjectColorScript)) as ObjectColorScript[]);
+
+        foreach (ObjectColorScript c in scriptList)
+        {
+            c.activeMaterial = colorMaterial;
+        }
+    }
+}
---
+++ AR_Adventskalender_Project/Assets/ScriptColorPicker/ObjectColorScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material activeMaterial;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (activeMaterial != null)
+            {
+                GetComponent<Renderer>().material = activeMaterial;
+            }
+        }
+    }
+}
