f8ba56fd8944156e3c8e0c05540d83daddca8bb9
Denzell92 <s0558978@htw-berlin.de>
2021-01-12 22:16:17 +0100

    candles can be lighted, christmas wreath is on screen with nice background

---
+++ AR_Adventskalender_Project/Assets/Scripts Adventskalender/CandleScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem fire;
+    public bool flameIsOn = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ParticleSystem.SizeOverLifetimeModule a = fire.sizeOverLifetime;
+        Debug.Log(a.x.curveMax);
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            /*if (flameIsOn)
+            {
+                var ps = fire.main;
+                ps.startLifetime = 0;
+            } else
+            {
+                var ps = fire.main;
+                ps.startLifetime = 5;
+                
+
+            }*/
+            ParticleSystem.EmissionModule emissionFire = fire.emission;
+            if (emissionFire.enabled)
+            {
+                emissionFire.enabled = false;
+            } else
+            {
+                emissionFire.enabled = true;
+            }
+
+        }
+    }
+}
