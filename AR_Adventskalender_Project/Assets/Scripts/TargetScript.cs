86f6b32142f4f6658453fbb484f818cecf546acd
Denzell92 <s0558978@htw-berlin.de>
2021-01-16 00:06:51 +0100

    when enemy touches the target scene will be refreshed

---
+++ AR_Adventskalender_Project/Assets/Scripts/TargetScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TargetScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.gameObject.name == "Enemy")
+        {
+            Debug.Log("Kommt da Was?");
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Tell me something");
+        if (other.transform.gameObject.name == "Enemy")
+        {
+            Debug.Log("Kommt da Was?");
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
