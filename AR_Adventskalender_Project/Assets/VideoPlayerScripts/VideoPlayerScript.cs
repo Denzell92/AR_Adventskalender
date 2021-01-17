34caace9dedb7be622a6e80f74e2c77cced07240
Denzell92 <s0558978@htw-berlin.de>
2021-01-17 16:30:53 +0100

    videoplayer in ar is ready to test

---
+++ AR_Adventskalender_Project/Assets/VideoPlayerScripts/VideoPlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    VideoPlayer videoController;
+    void Start()
+    {
+        videoController = GetComponent<VideoPlayer>();
+        videoController.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (videoController.isPlaying)
+        {
+            videoController.Pause();
+        } else if (!videoController.isPlaying)
+        {
+            videoController.Play();
+        }
+    }
+}
