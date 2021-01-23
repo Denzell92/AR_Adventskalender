6457a8c6010d4251ba2cd393f7688fd482d55789
asdernr <eduardseiler23@gmail.com>
2021-01-23 19:24:32 +0100

    repaired

---
+++ AR_Adventskalender_Project/Assets/ShakeDetector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Controller))]
+public class ShakeDetector : MonoBehaviour
+{
+    public float shakeDetectionThreshold;
+    public float minShakeInterval;
+
+    private float _sqrShakeDetectionThreshold;
+    private float _timeSinceLastShake;
+
+    private Controller _controller;
+
+    void Start()
+    {
+        _sqrShakeDetectionThreshold = Mathf.Pow(shakeDetectionThreshold, 2);
+        _controller = GetComponent<Controller>();
+    }
+
+    void Update()
+    {
+        if (Input.acceleration.sqrMagnitude >= _sqrShakeDetectionThreshold &&
+            Time.unscaledTime >= _timeSinceLastShake + minShakeInterval)
+        {
+            // _controller.ShakeRigidBody(Input.acceleration);
+            // _timeSinceLastShake = Time.unscaledTime;
+            _controller.ActivateSnow();
+        }
+    }
+}
